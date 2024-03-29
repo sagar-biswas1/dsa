d4012bed6fde006a497ab73619c4ef7e336b2e94
sagar-biswas1 <bsagor99@gmail.com>
2024-03-29 13:19:00 +0600

    priority_queue and map explored

---
+++ stl_map/stl_map.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map <string,int> mp;
+    mp.insert({"sam",1});
+    mp.insert({"ram",1});
+if(mp.count("sa")){
+cout<< mp["sam"]<<endl;
+
+}
+
+for(auto it= mp.begin(); it!= mp.end(); it++)
+{
+    cout<< it->first<<endl;
+}
+    return 0;
+}
