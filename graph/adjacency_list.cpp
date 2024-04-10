64a55d5e4ecf4871db442ca4b89bce9a62b970df
sagar-biswas1 <bsagor99@gmail.com>
2024-04-10 19:45:25 +0600

    exploring graph

---
+++ graph/adjacency_list.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<int> mat[n];
+    memset(mat, 0, sizeof(mat));
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        mat[a].push_back(b);
+        mat[b].push_back(a);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+cout<<i<<"--> "; 
+        for (int j = 0; j < mat[i].size(); j++)
+        {
+            cout << mat[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    if (mat[3][4] == 1)
+        cout << "connection ache" << endl;
+    cout << "❤ hello Nila ❤";
+    return 0;
+}
---
+++ graph/adjacency_matrix.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    int mat[n][n];
+    memset(mat, 0, sizeof(mat));
+    for (int i = 0; i < e; i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        mat[a][b] =1;
+        mat[b][a] = 1;
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout<< mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    if(mat[3][4]==1)cout<< "connection ache"<<endl;
+    cout <<"❤ hello Nila ❤";
+    return 0;
+}
